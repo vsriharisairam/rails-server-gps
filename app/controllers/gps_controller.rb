007cf431bd217bce586ba770b0955d3f8e846999
srihari sairam v <vsriharisairam@gmail.com>
2016-10-08 18:42:05 +0000

    server tcp

--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -10,9 +10,7 @@ class GpsController < ApplicationController
 server = TCPServer.open(2000)  # Socket to listen on port 2000
 loop {                         # Servers run forever
   client = server.accept       # Wait for a client to connect
-  client.puts(Time.now.ctime)# Send the time to the client
-  @respons = client.gets
-  client.puts "Closing the connection. Bye!"
+  @respons = client.gets(10)
   client.close                 # Disconnect from the client
 }
   end
